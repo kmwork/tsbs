3581360183d21c3c00eb135e90278f7cb1f48858
Konstantin Kakunin (datana) <kakunin@lanit.ru>
2020-04-29 16:10:12 +0300

    оптимизация массивов

--- kostya-bin/kostya-gen-yandex.sh
+++ kostya-bin/kostya-gen-yandex.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -v
-./tsbs_generate_data --use-case="devops" --seed=500 --scale=10 \
+./tsbs_generate_data --use-case="devops" --seed=5 --scale=10 \
     --timestamp-start="2020-04-01T00:00:00Z" \
     --timestamp-end="2020-04-01T00:01:00Z" \
     --log-interval="30s" --format="cassandra" \
