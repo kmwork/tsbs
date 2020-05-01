e451b6d38cf4586b973c91c78e62c74260970f60
Konstantin Kakunin (datana) <kakunin@lanit.ru>
2020-05-01 10:38:07 +0300

    пред-финишный дебаг

--- internal/utils/config.go
+++ internal/utils/config.go
@@ -1,28 +1,20 @@
 package utils
 
 import (
+	"flag"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
+	"log"
 )
 
-type KostyaConfig struct {
-	KostyaColumnCounter int64 `mapstructure:"KostyaColumnCounter"`
-}
-
-var config KostyaConfig
+var kostyaColumnCounter = flag.Int64("KostyaColumnCounter", 5000, "[Kostya-Author] Counter of table columns for 'CPU' (default 5000)")
 
 func init() {
-	config.AddToFlagSet(pflag.CommandLine)
-	pflag.Parse()
-}
-
-// AddToFlagSet adds command line flags needed by the BenchmarkRunnerConfig to the flag set.
-func (c KostyaConfig) AddToFlagSet(fs *pflag.FlagSet) {
-	fs.Int64("KostyaColumnCounter", 5000, "[Kostya-Author] size ('width') of table 'CPU' ")
+	log.Printf("[Config:Common] kostyaColumnCounter = %d", KostyaColumnCounter())
 }
 
 func KostyaColumnCounter() int64 {
-	return config.KostyaColumnCounter
+	return *kostyaColumnCounter
 }
 
 // SetupConfigFile defines the settings for the configuration file support.
