6160483d31a81fa1c7bed3d64751ed3b9adde583
Konstantin Kakunin (datana) <kakunin@lanit.ru>
2020-05-04 20:33:36 +0300

    перевел в PreConstructor

--- internal/utils/config.go
+++ internal/utils/config.go
@@ -12,8 +12,8 @@ var kostyaColumnCounter int64
 
 func PreConstructor() {
 	log.Printf("[Config:Common] os.Args = %v", os.Args)
-	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
-	flag.Int64Var(&kostyaColumnCounter, "kostya-count-of-columns", 5000, "[Kostya-Author] Counter of table columns for 'CPU' (default 5000)")
+	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
+	flag.Int64Var(&kostyaColumnCounter, "KostyaСountOfColumns", 5000, "[Kostya-Author] Counter of table columns for 'CPU' (default 5000)")
 	flag.Parse()
 	log.Printf("[Config:PreConstructor] kostyaColumnCounter = %d", kostyaColumnCounter)
 }
--- kostya-bin/kostya-gen-yandex.sh
+++ kostya-bin/kostya-gen-yandex.sh
@@ -1,8 +1,9 @@
 #!/bin/sh -v
 clear
-./tsbs_generate_data --use-case="devops" --seed=500 --scale=1000 \
+./tsbs_generate_data --KostyaСountOfColumns=15000 \
+    --use-case="devops" --seed=500 --scale=1000 \
     --timestamp-start="2020-04-01T00:00:00Z" \
     --timestamp-end="2020-04-01T00:01:00Z" \
     --log-interval="30s" --format="clickhouse" \
-    --kostya-count-of-columns=15000 \
+    --KostyaСountOfColumns=15000 \
     | gzip > /my-ext4/yandex-data-t4.gz
